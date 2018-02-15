7475a60ee9257a6486f7269e9f026910e96e45a6
ZhangYuu <memo40k@outlook.com>
2018-02-14 20:40:19 -0500

    add flask server

--- study_2.py
+++ study_2.py
@@ -1,7 +1,12 @@
 import hashlib
 import json
 from time import time
-
+import hashlib
+import json
+from textwrap import dedent
+from time import time
+from uuid import uuid4
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
@@ -29,7 +34,6 @@ class Blockchain(object):
 
         # Reset the current list of transactions
         self.current_transactions = []
-
         self.chain.append(block)
         return block
 
@@ -60,7 +64,46 @@ class Blockchain(object):
         :param block: <dict> Block
         :return: <str>
         """
-
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': 'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
